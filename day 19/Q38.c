c66feb0f1ca4db95aae29a3a04eba4d459b53011
Ishi283 <ishiagarwal2829@gmail.com>
2025-09-15 23:26:50 +0530

    Add files via upload

---
+++ day 19/Q38.c
@@ -0,0 +1,31 @@
+//Q38: Write a program to find the sum of digits of a number.
+
+/*
+Sample Test Cases:
+Input 1:
+123
+Output 1:
+6
+
+Input 2:
+999
+Output 2:
+27
+
+*/
+#include<stdio.h>
+int main()
+{
+    int n,r;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    int sum =0;
+    while(n>0)
+    {
+        r=n%10;
+        sum=sum+r;
+        n=n/10;
+    }
+    printf("%d",sum);
+    return 0;
+}
