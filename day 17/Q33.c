8eb006199b80ca4e090990ee3bf900d402626e83
Ishi283 <ishiagarwal2829@gmail.com>
2025-09-15 23:24:30 +0530

    Add files via upload

--- day 17/Q33.c
+++ day 17/Q33.c
@@ -1,1 +1,45 @@
+//3: Write a program to check if a number is an Armstrong number.
 
+/*
+Sample Test Cases:
+Input 1:
+153
+Output 1:
+Armstrong
+
+Input 2:
+123
+Output 2:
+Not Armstrong
+
+*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,c=0,og;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    og=n;
+    while(n>0)
+    {
+        n=n/10;
+        c++;
+    }
+    n=og;
+    int sum=0;
+    while(n>0)
+    {
+        sum=sum+pow(n%10,c);
+        n=n/10;
+    }
+    if(sum==og)
+    {
+        printf("Amstrong no.");
+    }
+    else
+    {
+        printf("Not a amstrong no.");
+    }
+    return 0;
+}
