082295d54ced5d84449fc5d9627239cf06545b46
Ishi283 <ishiagarwal2829@gmail.com>
2025-10-01 14:21:38 +0530

    Add files via upload

---
+++ day 26/Q52.c
@@ -0,0 +1,42 @@
+/* Q52 Write a program to print the following pattern.
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+*/
+#include<stdio.h>
+int main()
+{
+    printf("*\n");
+    printf("\n");
+    for(int i=1;i<=3;i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+    for(int i=1;i<=5;i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+    for(int i=1;i<=3;i++)
+    {
+        printf("*\n");
+    }
+    printf("\n");
+    printf("*");
+    return 0;
+}
