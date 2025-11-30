8a4d75768ba4dc62327d74aaaef59bc7984f1982
Ishi283 <ishiagarwal2829@gmail.com>
2025-11-30 23:43:20 +0530

    Add files via upload

--- day 78/Q128.c
+++ day 78/Q128.c
@@ -1,1 +1,49 @@
+//Q128: Read a text file and count how many vowels and consonants are in the file. Ignore digits and special characters.
 
+/*
+Sample Test Cases:
+Input 1:
+File: text.txt (Content: Welcome to C language)
+Output 1:
+Vowels: 8
+Consonants: 10
+
+*/
+#include <stdio.h>
+int main() 
+{
+    FILE *file;
+    char filename[50];
+    char ch;
+    int vowels = 0, consonants = 0;    
+    printf("Enter filename: ");
+    scanf("%s", filename);
+    file = fopen(filename, "r");
+    if (file == NULL) 
+    {
+        printf("Error: Could not open file!\n");
+        return 1;
+    }
+    while ((ch = fgetc(file)) != EOF) 
+    {
+        if (ch >= 'A' && ch <= 'Z') 
+        {
+            ch = ch + 32;
+        }
+        if (ch >= 'a' && ch <= 'z') 
+        {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowels++;
+            }
+             else 
+             {
+                consonants++;
+            }
+        }
+    }
+    fclose(file);
+    printf("\nFile Analysis:\n");
+    printf("Vowels: %d\n", vowels);
+    printf("Consonants: %d\n", consonants);
+    return 0;
+}
