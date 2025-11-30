952bf1f434b13923311d685975abd4faba96786c
Ishi283 <ishiagarwal2829@gmail.com>
2025-12-01 01:42:00 +0530

    Add files via upload

--- day 93/Q143.c
+++ day 93/Q143.c
@@ -1,1 +1,45 @@
+//Q143: Find and print the student with the highest marks.
 
+/*
+Sample Test Cases:
+Input 1:
+3 students: Ravi 101 95, Sita 102 85, Aman 103 88
+Output 1:
+Topper: Ravi (Marks: 95)
+
+*/
+#include <stdio.h>
+struct Student 
+{
+    char name[50];
+    int roll_no;
+    float marks;
+};
+int main() 
+{
+    struct Student students[5];
+    int i, max_index = 0;    
+    printf("Enter details of 5 students:\n");
+    for (i = 0; i < 5; i++) 
+    {
+        printf("\nStudent %d:\n", i + 1);    
+        printf("Name: ");
+        scanf("%s", students[i].name);
+        printf("Roll Number: ");
+        scanf("%d", &students[i].roll_no);
+        printf("Marks: ");
+        scanf("%f", &students[i].marks);
+    }
+    for (i = 1; i < 5; i++) 
+    {
+        if (students[i].marks > students[max_index].marks) 
+        {
+            max_index = i;
+        }
+    }
+    printf("\n\nStudent with Highest Marks:\n");
+    printf("Name: %s\n", students[max_index].name);
+    printf("Roll Number: %d\n", students[max_index].roll_no);
+    printf("Marks: %.2f\n", students[max_index].marks);
+    return 0;
+}
