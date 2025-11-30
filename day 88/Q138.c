b938b0623d1fbc6a2e355ee754fbc90b61f066ea
Ishi283 <ishiagarwal2829@gmail.com>
2025-12-01 01:16:44 +0530

    Add files via upload

--- day 88/Q138.c
+++ day 88/Q138.c
@@ -1,1 +1,48 @@
+//Q138: Print all enum names and integer values using a loop.
 
+/*
+Sample Test Cases:
+Input 1:
+No input
+Output 1:
+RED=0
+YELLOW=1
+GREEN=2
+
+*/
+#include <stdio.h>
+enum Color 
+{
+    RED,
+    GREEN,
+    BLUE,
+    YELLOW,
+    ORANGE
+};
+int main() 
+{
+    printf("Enum Color values:\n");
+    for (int i = RED; i <= ORANGE; i++) 
+    {
+        printf("Color %d: ", i);
+        switch(i) 
+        {
+            case RED:
+                printf("RED\n");
+                break;
+            case GREEN:
+                printf("GREEN\n");
+                break;
+            case BLUE:
+                printf("BLUE\n");
+                break;
+            case YELLOW:
+                printf("YELLOW\n");
+                break;
+            case ORANGE:
+                printf("ORANGE\n");
+                break;
+        }
+    }
+    return 0;
+}
